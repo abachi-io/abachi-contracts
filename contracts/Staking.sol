6531cd9f7669b76f1dee5f6d4a035f6109748fe7
admin <admin@cryptostat.org>
2022-01-16 21:46:10 +1100

    update contracts

---
+++ contracts/Staking.sol
@@ -0,0 +1,309 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity ^0.7.5;
+
+import "./libraries/SafeMath.sol";
+import "./libraries/SafeERC20.sol";
+
+import "./interfaces/IERC20.sol";
+import "./interfaces/IsABI.sol";
+import "./interfaces/IgABI.sol";
+import "./interfaces/IDistributor.sol";
+
+import "./types/AbachiAccessControlled.sol";
+
+contract AbachiStaking is AbachiAccessControlled {
+    /* ========== DEPENDENCIES ========== */
+
+    using SafeMath for uint256;
+    using SafeERC20 for IERC20;
+    using SafeERC20 for IsABI;
+    using SafeERC20 for IgABI;
+
+    /* ========== EVENTS ========== */
+
+    event DistributorSet(address distributor);
+    event WarmupSet(uint256 warmup);
+
+    /* ========== DATA STRUCTURES ========== */
+
+    struct Epoch {
+        uint256 length; // in seconds
+        uint256 number; // since inception
+        uint256 end; // timestamp
+        uint256 distribute; // amount
+    }
+
+    struct Claim {
+        uint256 deposit; // if forfeiting
+        uint256 gons; // staked balance
+        uint256 expiry; // end of warmup period
+        bool lock; // prevents malicious delays for claim
+    }
+
+    /* ========== STATE VARIABLES ========== */
+
+    IERC20 public immutable ABI;
+    IsABI public immutable sABI;
+    IgABI public immutable gABI;
+
+    Epoch public epoch;
+
+    IDistributor public distributor;
+
+    mapping(address => Claim) public warmupInfo;
+    uint256 public warmupPeriod;
+    uint256 private gonsInWarmup;
+
+    /* ========== CONSTRUCTOR ========== */
+
+    constructor(
+        address _abi,
+        address _sABI,
+        address _gABI,
+        uint256 _epochLength,
+        uint256 _firstEpochNumber,
+        uint256 _firstEpochTime,
+        address _authority
+    ) AbachiAccessControlled(IAbachiAuthority(_authority)) {
+        require(_abi != address(0), "Zero address: ABI");
+        ABI = IERC20(_abi);
+        require(_sABI != address(0), "Zero address: sABI");
+        sABI = IsABI(_sABI);
+        require(_gABI != address(0), "Zero address: gABI");
+        gABI = IgABI(_gABI);
+
+        epoch = Epoch({length: _epochLength, number: _firstEpochNumber, end: _firstEpochTime, distribute: 0});
+    }
+
+    /* ========== MUTATIVE FUNCTIONS ========== */
+
+    /**
+     * @notice stake ABI to enter warmup
+     * @param _to address
+     * @param _amount uint
+     * @param _claim bool
+     * @param _rebasing bool
+     * @return uint
+     */
+    function stake(
+        address _to,
+        uint256 _amount,
+        bool _rebasing,
+        bool _claim
+    ) external returns (uint256) {
+        ABI.safeTransferFrom(msg.sender, address(this), _amount);
+        _amount = _amount.add(rebase()); // add bounty if rebase occurred
+        if (_claim && warmupPeriod == 0) {
+            return _send(_to, _amount, _rebasing);
+        } else {
+            Claim memory info = warmupInfo[_to];
+            if (!info.lock) {
+                require(_to == msg.sender, "External deposits for account are locked");
+            }
+
+            warmupInfo[_to] = Claim({
+                deposit: info.deposit.add(_amount),
+                gons: info.gons.add(sABI.gonsForBalance(_amount)),
+                expiry: epoch.number.add(warmupPeriod),
+                lock: info.lock
+            });
+
+            gonsInWarmup = gonsInWarmup.add(sABI.gonsForBalance(_amount));
+
+            return _amount;
+        }
+    }
+
+    /**
+     * @notice retrieve stake from warmup
+     * @param _to address
+     * @param _rebasing bool
+     * @return uint
+     */
+    function claim(address _to, bool _rebasing) public returns (uint256) {
+        Claim memory info = warmupInfo[_to];
+
+        if (!info.lock) {
+            require(_to == msg.sender, "External claims for account are locked");
+        }
+
+        if (epoch.number >= info.expiry && info.expiry != 0) {
+            delete warmupInfo[_to];
+
+            gonsInWarmup = gonsInWarmup.sub(info.gons);
+
+            return _send(_to, sABI.balanceForGons(info.gons), _rebasing);
+        }
+        return 0;
+    }
+
+    /**
+     * @notice forfeit stake and retrieve ABI
+     * @return uint
+     */
+    function forfeit() external returns (uint256) {
+        Claim memory info = warmupInfo[msg.sender];
+        delete warmupInfo[msg.sender];
+
+        gonsInWarmup = gonsInWarmup.sub(info.gons);
+
+        ABI.safeTransfer(msg.sender, info.deposit);
+
+        return info.deposit;
+    }
+
+    /**
+     * @notice prevent new deposits or claims from ext. address (protection from malicious activity)
+     */
+    function toggleLock() external {
+        warmupInfo[msg.sender].lock = !warmupInfo[msg.sender].lock;
+    }
+
+    /**
+     * @notice redeem sABI for ABIs
+     * @param _to address
+     * @param _amount uint
+     * @param _trigger bool
+     * @param _rebasing bool
+     * @return amount_ uint
+     */
+    function unstake(
+        address _to,
+        uint256 _amount,
+        bool _trigger,
+        bool _rebasing
+    ) external returns (uint256 amount_) {
+        amount_ = _amount;
+        uint256 bounty;
+        if (_trigger) {
+            bounty = rebase();
+        }
+        if (_rebasing) {
+            sABI.safeTransferFrom(msg.sender, address(this), _amount);
+            amount_ = amount_.add(bounty);
+        } else {
+            gABI.burn(msg.sender, _amount); // amount was given in gABI terms
+            amount_ = gABI.balanceFrom(amount_).add(bounty); // convert amount to ABI terms & add bounty
+        }
+
+        require(amount_ <= ABI.balanceOf(address(this)), "Insufficient ABI balance in contract");
+        ABI.safeTransfer(_to, amount_);
+    }
+
+    /**
+     * @notice convert _amount sABI into gBalance_ gABI
+     * @param _to address
+     * @param _amount uint
+     * @return gBalance_ uint
+     */
+    function wrap(address _to, uint256 _amount) external returns (uint256 gBalance_) {
+        sABI.safeTransferFrom(msg.sender, address(this), _amount);
+        gBalance_ = gABI.balanceTo(_amount);
+        gABI.mint(_to, gBalance_);
+    }
+
+    /**
+     * @notice convert _amount gABI into sBalance_ sABI
+     * @param _to address
+     * @param _amount uint
+     * @return sBalance_ uint
+     */
+    function unwrap(address _to, uint256 _amount) external returns (uint256 sBalance_) {
+        gABI.burn(msg.sender, _amount);
+        sBalance_ = gABI.balanceFrom(_amount);
+        sABI.safeTransfer(_to, sBalance_);
+    }
+
+    /**
+     * @notice trigger rebase if epoch over
+     * @return uint256
+     */
+    function rebase() public returns (uint256) {
+        uint256 bounty;
+        if (epoch.end <= block.timestamp) {
+            sABI.rebase(epoch.distribute, epoch.number);
+
+            epoch.end = epoch.end.add(epoch.length);
+            epoch.number++;
+
+            if (address(distributor) != address(0)) {
+                distributor.distribute();
+                bounty = distributor.retrieveBounty(); // Will mint abi for this contract if there exists a bounty
+            }
+            uint256 balance = ABI.balanceOf(address(this));
+            uint256 staked = sABI.circulatingSupply();
+            if (balance <= staked.add(bounty)) {
+                epoch.distribute = 0;
+            } else {
+                epoch.distribute = balance.sub(staked).sub(bounty);
+            }
+        }
+        return bounty;
+    }
+
+    /* ========== INTERNAL FUNCTIONS ========== */
+
+    /**
+     * @notice send staker their amount as sABI or gABI
+     * @param _to address
+     * @param _amount uint
+     * @param _rebasing bool
+     */
+    function _send(
+        address _to,
+        uint256 _amount,
+        bool _rebasing
+    ) internal returns (uint256) {
+        if (_rebasing) {
+            sABI.safeTransfer(_to, _amount); // send as sABI (equal unit as ABI)
+            return _amount;
+        } else {
+            gABI.mint(_to, gABI.balanceTo(_amount)); // send as gABI (convert units from ABI)
+            return gABI.balanceTo(_amount);
+        }
+    }
+
+    /* ========== VIEW FUNCTIONS ========== */
+
+    /**
+     * @notice returns the sABI index, which tracks rebase growth
+     * @return uint
+     */
+    function index() public view returns (uint256) {
+        return sABI.index();
+    }
+
+    /**
+     * @notice total supply in warmup
+     */
+    function supplyInWarmup() public view returns (uint256) {
+        return sABI.balanceForGons(gonsInWarmup);
+    }
+
+    /**
+     * @notice seconds until the next epoch begins
+     */
+    function secondsToNextEpoch() external view returns (uint256) {
+        return epoch.end.sub(block.timestamp);
+    }
+
+    /* ========== MANAGERIAL FUNCTIONS ========== */
+
+    /**
+     * @notice sets the contract address for LP staking
+     * @param _distributor address
+     */
+    function setDistributor(address _distributor) external onlyGovernor {
+        distributor = IDistributor(_distributor);
+        emit DistributorSet(_distributor);
+    }
+
+    /**
+     * @notice set warmup period for new stakers
+     * @param _warmupPeriod uint
+     */
+    function setWarmupLength(uint256 _warmupPeriod) external onlyGovernor {
+        warmupPeriod = _warmupPeriod;
+        emit WarmupSet(_warmupPeriod);
+    }
+}
---
+++ contracts/TreasuryNote.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity ^0.7.5;
+
+import "./libraries/SafeMath.sol";
+
+import "./interfaces/IERC20.sol";
+import "./interfaces/IABI.sol";
+import "./interfaces/IERC20Permit.sol";
+
+import "./types/ERC20Permit.sol";
+import "./types/AbachiAccessControlled.sol";
+
+contract TreasuryNote is ERC20Permit, IABI, AbachiAccessControlled {
+    using SafeMath for uint256;
+    string public backedAsset = "DAI";
+
+    constructor(address _authority)
+    ERC20("Treasury Note", "tNote", 18)
+    ERC20Permit("Treasury Note")
+    AbachiAccessControlled(IAbachiAuthority(_authority)) {}
+
+    function mint(address account_, uint256 amount_) external override onlyGovernor {
+        _mint(account_, amount_);
+    }
+
+    function burn(uint256 amount) external override {
+        _burn(msg.sender, amount);
+    }
+
+    function burnFrom(address account_, uint256 amount_) external override {
+        _burnFrom(account_, amount_);
+    }
+
+    function _burnFrom(address account_, uint256 amount_) internal {
+        uint256 decreasedAllowance_ = allowance(account_, msg.sender).sub(amount_, "ERC20: burn amount exceeds allowance");
+
+        _approve(account_, msg.sender, decreasedAllowance_);
+        _burn(account_, amount_);
+    }
+}
